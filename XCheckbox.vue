16fe9659f38b179c9bb583a70734a2be2cccb3e1
wanjie <wanjie@oushu.io>
2021-12-22 19:13:46 +0800

    feat: 修改checkbox样式

--- XCheckbox.vue
+++ XCheckbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-checkbox>
+  <a-checkbox class="smartui-checkbox">
     <template v-for="item in slots" v-slot:[item]>
       <slot :name="item"></slot>
     </template>
@@ -19,6 +19,6 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-
+<style lang="scss">
+@import "./styles/components/XCheckbox.scss";
 </style>
