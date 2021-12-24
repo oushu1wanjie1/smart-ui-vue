557971155f494791a4c424296a4f826a7c3824b9
wanjie <wanjie@oushu.io>
2021-12-24 15:01:47 +0800

    fix: x-table样式调整

---
+++ XA.vue
@@ -0,0 +1,15 @@
+<template>
+  <a class="smartui-a">
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "XA"
+}
+</script>
+
+<style lang="scss">
+@import './styles/components/XA.scss'
+</style>
