d165844566396296111856cfd0cb9878e30ed44b
tan_qiang <tan.qiang@oushu.io>
2022-01-05 16:43:42 +0800

    feat: 时间输入框 数字输入框

--- XDatePicker.vue
+++ XDatePicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-date-picker>
+  <a-date-picker :class="`smartui-date-picker ${wrapperClass}`" v-bind="{ ...props }" :style="wrapperStyle">
     <template v-for="item in slots" v-slot:[item]>
       <slot :name="item"></slot>
     </template>
@@ -7,13 +7,22 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, Prop } from 'vue'
+import { computed, defineComponent, Prop, ref } from 'vue'
+import { DatePicker } from 'ant-design-vue'
 
 export default defineComponent({
   name: 'XDatePicker',
+  props: {
+    ...DatePicker.props
+  },
   setup(props, context) {
+    const wrapperClass = ref(context.attrs.class || '')
+    const wrapperStyle = ref(context.attrs.style)
     return {
-      slots: computed(() => Object.keys(context.slots))
+      slots: computed(() => Object.keys(context.slots)),
+      wrapperStyle,
+      wrapperClass,
+      props,
     }
   }
 })
