474b6c56d21b2821f3e69d77f2c83f626e3fc427
tan_qiang <tan.qiang@oushu.io>
2022-01-11 15:49:58 +0800

    style: drawer 背景色; antd less变量修改

---
+++ modifyVars.ts
@@ -0,0 +1,22 @@
+module.exports = {
+  // 全局主色
+  'primary-color': '#336CFF',
+  // 成功色
+  'success-color': 'rgba(75, 202, 173, 1)',
+  // 警告色
+  'warning-color': 'rgba(215, 201, 68, 1)',
+  // 错误色
+  'error-color': 'rgba(215, 68, 114, 1)',
+  // 主文本色
+  'text-color': '#282B2E',
+  // heading文本色
+  'heading-color': '#282B2E',
+  // 行高
+  'line-height-base': '1.4',
+  // 组件/浮层圆角
+  'border-radius-base': '4px',
+  // 边框色
+  'border-color-base': '#D5D8DB',
+  // Tooltip max width
+  'tooltip-max-width': '200px',
+}
