eb278c5d8c4f23d76a6effb560d560e5a766d7ac
wanjie <wanjie@oushu.io>
2022-01-20 22:51:32 +0800

    fix: 修复一些问题

--- XDrawer.vue
+++ XDrawer.vue
@@ -1,22 +1,31 @@
 <template>
-  <a-drawer wrapper-class-name="smartui-drawer" :fullScreen="fullScreen">
-    <template v-for="item in slots" v-slot:[item]>
+  <a-drawer wrapper-class-name="smartui-drawer" :fullScreen="fullScreen" :closable="closable">
+    <template v-for="item in slots" :key="item" v-slot:[item]>
       <slot :name="item"></slot>
+      <div class="close-btn-wrapper" v-if="item === 'default' && closable">
+        <icon name="ui-date-pick/close" class="close-icon"/>
+      </div>
     </template>
   </a-drawer>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, Prop } from 'vue'
+import { computed, defineComponent } from 'vue'
+import Icon from '@/components/Icon.vue'
 
 export default defineComponent({
   name: 'XDrawer',
+  components: { Icon },
   props: {
     // 默认离top 74px（header的高度）
     // 需要 top 为 0 设置本属性即可
     fullScreen: {
       type: Boolean,
       default: false
+    },
+    closable: {
+      type: Boolean,
+      default: true
     }
   },
   setup(props, context) {
