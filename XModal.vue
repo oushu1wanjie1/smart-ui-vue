9f62e1d8a05dbe80d6e415c23d9c88b7fabc7702
tan_qiang <tan.qiang@oushu.io>
2022-03-29 18:49:43 +0800

    feat：modal 添加 props描述；modal 开启默认居中

--- XModal.vue
+++ XModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-modal dialogClass="x-modal">
+  <a-modal dialogClass="x-modal" v-bind="{...$props}">
     <template v-for="item in slots" v-slot:[item]>
       <slot :name="item"></slot>
     </template>
@@ -7,10 +7,89 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, Prop } from 'vue'
+import { computed, CSSProperties, defineComponent, PropType } from 'vue'
+import type { ButtonProps as ButtonPropsType, LegacyButtonType } from 'ant-design-vue/es/button/buttonTypes'
+
+type getContainerFunc = () => HTMLElement;
 
 export default defineComponent({
   name: 'XModal',
+  props: {
+    prefixCls: String,
+    visible: { type: Boolean, default: false },
+    confirmLoading: { type: Boolean, default: false },
+    /**
+     * 也可以是 slot
+     */
+    title: String,
+    closable: { type: Boolean, default: true },
+    /**
+     * vnode or slot
+     */
+    closeIcon: Object,
+    onOk: Function as PropType<(e: MouseEvent) => void>,
+    onCancel: Function as PropType<(e: MouseEvent) => void>,
+    'onUpdate:visible': Function as PropType<(visible: boolean) => void>,
+    onChange: Function as PropType<(visible: boolean) => void>,
+    afterClose: Function as PropType<() => void>,
+    /**
+     * 默认居中
+     */
+    centered: { type: Boolean, default: true },
+    width: {
+      type: [String, Number],
+      default: 400
+    },
+    /**
+     * 底部内容，当不需要默认底部按钮时，可以设为 :footer="null"
+     * 也可以是 slot
+     * 默认值：确定取消按钮
+     */
+    footer: String,
+    /**
+     * 确定按钮文字
+     * 也可以是 slot
+     */
+    okText: String,
+    okType: {
+      type: String as PropType<LegacyButtonType>,
+      default: 'primary'
+    },
+    /**
+     * 关闭按钮文字
+     * 也可以是 slot
+     */
+    cancelText: String,
+    /**
+     * 自定义图标
+     * 	VNode | ()=>VNode
+     */
+    icon: Object,
+    maskClosable: { type: Boolean, default: true },
+    forceRender: { type: Boolean, default: false },
+    okButtonProps: Object as PropType<ButtonPropsType>,
+    cancelButtonProps: Object as PropType<ButtonPropsType>,
+    destroyOnClose: { type: Boolean, default: false },
+    wrapClassName: String,
+    maskTransitionName: {
+      type: String,
+      default: 'fade',
+    },
+    transitionName: {
+      type: String,
+      default: 'zoom',
+    },
+    getContainer: {
+      type: [String, Function, Boolean, Object] as PropType<string | HTMLElement | getContainerFunc | false>,
+      default: () => document.body,
+    },
+    zIndex: Number,
+    bodyStyle: { type: Object as PropType<CSSProperties>, default: undefined as unknown as CSSProperties },
+    maskStyle: { type: Object as PropType<CSSProperties>, default: undefined as unknown as CSSProperties },
+    mask: { type: Boolean, default: true },
+    keyboard: { type: Boolean, default: true },
+    wrapProps: Object,
+  },
   setup(props, context) {
     return {
       slots: computed(() => Object.keys(context.slots))
