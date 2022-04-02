560b5f28856002a3a37bf7a9d7b964fd62fa235a
wanjie <wanjie@oushu.io>
2022-04-02 14:58:59 +0800

    fix: 修复x-input-number不限制最大最小值的问题

--- XInputNumber.vue
+++ XInputNumber.vue
@@ -1,15 +1,22 @@
 <template>
-  <a-input-number v-model:value="value" v-bind="{ ...props }" :class="`smartui-number-input`" :style="{...wrapperStyle}">
+  <a-input-number
+    v-model:value="value"
+    v-bind="{ ...props }"
+    :class="`smartui-number-input`"
+    :style="{...wrapperStyle}" @change="handleChange"
+    max-length="3"
+  >
   </a-input-number>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, toRefs, watch } from 'vue'
 import { InputNumber } from 'ant-design-vue'
 import { useModel } from '@/smart-ui-vue/utils'
 
 export default {
   name: 'XInputNumber',
+  emits: ['change'],
   props: {
     inputWidth: {
       type: String,
@@ -19,14 +26,26 @@ export default {
   },
   setup(props, context) {
     const wrapperStyle = ref(context.attrs.style)
+    const { max: maxRef, min: minRef } = toRefs(props)
+    const value = useModel('value', props, context)
     if (wrapperStyle.value && wrapperStyle.value.width === undefined) {
       wrapperStyle.value.width = '120px'
+    }
+
+    watch(value, (val, oldVal) => {
+      if (isNaN(val)) value.value = oldVal
+    })
+    const handleChange = (val) => {
+      if (maxRef.value !== undefined && val > maxRef.value) value.value = maxRef.value
+      if (minRef.value !== undefined && val < minRef.value) value.value = minRef.value
+      context.emit('change', val)
     }
 
     return {
       props,
       wrapperStyle,
-      value: useModel('value', props, context),
+      value,
+      handleChange
     }
   }
 }
