c76965d5d07e5edf390e3594b0f5ae13c204c246
wanjie <wanjie@oushu.io>
2022-08-10 17:27:25 +0800

    feat: 添加form方法

--- XForm.vue
+++ XForm.vue
@@ -1,13 +1,43 @@
 <template>
-  <a-form class="smartui-form">
+  <a-form ref="antdForm" class="smartui-form">
     <slot></slot>
   </a-form>
 </template>
 
-<script>
-export default {
-  name: 'XForm'
-}
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { Form } from 'ant-design-vue'
+
+export default defineComponent({
+  name: 'XForm',
+  components: { AForm: Form },
+  setup() {
+    const antdForm = ref<Record<string, any> | null>(null)
+    const validate = () => {
+      if (antdForm.value) antdForm.value.validate.apply(this, arguments)
+    }
+    const validateFields = () => {
+      if (antdForm.value) antdForm.value.validateFields.apply(this, arguments)
+    }
+    const scrollToField = () => {
+      if (antdForm.value) antdForm.value.scrollToField.apply(this, arguments)
+    }
+    const resetFields = () => {
+      if (antdForm.value) antdForm.value.resetFields.apply(this, arguments)
+    }
+    const clearValidate = () => {
+      if (antdForm.value) antdForm.value.clearValidate.apply(this, arguments)
+    }
+    return {
+      antdForm,
+      validate,
+      validateFields,
+      scrollToField,
+      resetFields,
+      clearValidate
+    }
+  }
+})
 </script>
 
 <style lang="scss">
