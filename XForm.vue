eee092e89a424c2feb8af7920e8e7d8977523494
wanjie <wanjie@oushu.io>
2022-08-30 16:35:30 +0800

    fix conflict

--- XForm.vue
+++ XForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-form ref="formRef" v-bind="{ ...$props, ...$attrs}" class="smartui-form">
+  <a-form v-bind="{ ...$props, ...$attrs}" class="smartui-form" ref="formRef">
     <slot></slot>
   </a-form>
 </template>
@@ -20,19 +20,24 @@ export default defineComponent({
   setup() {
     const formRef = ref<Record<string, any> | null>(null)
     const validate = (...args: any[]) => {
-      if (formRef.value) formRef.value.validate.apply(this, args)
+      if (formRef.value) return formRef.value.validate.apply(this, args)
+      else return Promise.reject('未找到Antd Form实例')
     }
     const validateFields = (...args: any[]) => {
-      if (formRef.value) formRef.value.validateFields.apply(this, args)
+      if (formRef.value) return formRef.value.validateFields.apply(this, args)
+      else return Promise.reject('未找到Antd Form实例')
     }
     const scrollToField = (...args: any[]) => {
-      if (formRef.value) formRef.value.scrollToField.apply(this, args)
+      if (formRef.value) return formRef.value.scrollToField.apply(this, args)
+      else return Promise.reject('未找到Antd Form实例')
     }
     const resetFields = (...args: any[]) => {
-      if (formRef.value) formRef.value.resetFields.apply(this, args)
+      if (formRef.value) return formRef.value.resetFields.apply(this, args)
+      else return Promise.reject('未找到Antd Form实例')
     }
     const clearValidate = (...args: any[]) => {
-      if (formRef.value) formRef.value.clearValidate.apply(this, args)
+      if (formRef.value) return formRef.value.clearValidate.apply(this, args)
+      else return Promise.reject('未找到Antd Form实例')
     }
     return {
       formRef,
